880ad851ad5c4b59a4665148570ff194367a32cf
rfps09 <rafael09felipe@gmail.com>
2022-11-08 18:15:14 -0300

    Range Queries

---
+++ RangeQueries/DynamicRangeSumQueries.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+ll build(ll *heap, int *leafs, int l, int r, int node=1) {
+    if(l == r) return heap[node]=leafs[l-1];
+    int mid = (l+r)/2;
+    return heap[node] = build(heap,leafs,l,mid,node*2) + build(heap,leafs,mid+1,r,node*2+1);
+}
+
+ll query(ll *heap, int a, int b, int l, int r, int node=1) {
+    if(l >= a && r <= b) return heap[node];
+    if(a > r || b < l) return 0;
+    int mid = (l+r)/2;
+    return query(heap,a,b,l,mid,node*2) + query(heap,a,b,mid+1,r,node*2+1);
+}
+
+ll update(ll *heap, int k, int u, int l, int r, int node=1) {
+    if(k == l && k == r) return heap[node] = u;
+    if(k >= l && k <= r) {
+        int mid = (l+r)/2;
+        return heap[node] = update(heap,k,u,l,mid,node*2) + update(heap,k,u,mid+1,r,node*2+1);
+    }
+    return heap[node];
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    int n,q;
+    int type;
+    int a,b;
+
+    cin >> n >> q;
+
+    int vetor[n];
+    for(int i=0; i<n; i++) {
+        cin >> vetor[i];
+    }
+
+    ll heap[4*n];
+
+    build(heap,vetor,1,n);
+
+    for(int i=0; i<q; i++) {
+        cin >> type >> a >> b;
+        if(type-1) cout << query(heap,a,b,1,n) << "\n";
+        else update(heap,a,b,1,n);
+    }
+
+    return 0;
+}
